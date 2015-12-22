f432bf49e77ca64d3986b01e2ba5c106e4265028
sriram <salagappa@provocraft.com>
2015-12-22 03:58:06 -0700

    added classes

--- typescript-tryout/greeter.js
+++ typescript-tryout/greeter.js
@@ -1,4 +1,17 @@
+/*
 function greeter(person) {
     return "hello" + person;
 }
 document.body.innerHTML = greeter("sriram");
+*/
+var Greeter = (function () {
+    function Greeter(name) {
+        this.name = name;
+    }
+    Greeter.prototype.greet = function () {
+        return "hello" + this.name;
+    };
+    return Greeter;
+})();
+var greeter = new Greeter("Sriram");
+document.body.innerHTML = greeter.greet();
--- typescript-tryout/greeter.ts
+++ typescript-tryout/greeter.ts
@@ -1,9 +1,27 @@
-function greeter(person) {
 
-	return "hello" + person;	
+/*
+function greeter(person) {
+    return "hello" + person;
+}
+document.body.innerHTML = greeter("sriram");
+*/
+
+
+class Greeter
+{
+	name:String;
+
+	constructor(name:String){
+		this.name=name;
+	}
+	greet() {
+		return "hello" + this.name;
+	}
 }
 
 
+var greeter = new Greeter("Sriram");
 
-document.body.innerHTML = greeter("sriram");
+document.body.innerHTML =  greeter.greet();
+
 
